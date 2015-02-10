38ec4187ae41fb114b7f5918e20fb77132ca8e4d
JMGoldsmith <spartanaudio@gmail.com>
2015-02-09 20:59:24 -0600

    item DB working

--- app/models/item_data.rb
+++ app/models/item_data.rb
@@ -1,4 +1,15 @@
 class ItemData < ActiveRecord::Base
   belongs_to :auctions
   has_many :auction_and_items
+
+  def self.get_item_id_and_name(id)
+    url = ("http://us.battle.net/api/wow/item/#{id}")
+    response = HTTParty.get(url)
+    id = response['id']
+    name = response['name']
+    sell_price = response['sellPrice']
+    stacks = response['stackable']
+    buy_price = response['buyPrice']
+    ItemData.create({item_id: id, name: name, sellPrice: sell_price, stackable: stacks, buyPrice: buy_price })
+  end
 end
