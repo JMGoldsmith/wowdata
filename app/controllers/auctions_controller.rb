2477d19610d48792f437db6752278c9c88056178
JMGoldsmith <spartanaudio@gmail.com>
2015-02-16 17:24:30 -0600

    fix time on bottom

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -10,51 +10,42 @@ class AuctionsController < ApplicationController
     params_array = [] 
     params_array.push(params[:item_id])
     @buyout_data = connection.exec(%q[select date_trunc('hour', created_at), avg(buyout) from auctions where item_id = $1 group by 1 order by 1;],params_array)
-    @bid_data = connection.exec(%q[select date_trunc('hour', created_at), avg(buyout) from auctions where item_id = $1 group by 1 order by 1;],params_array)
+    @bid_data = connection.exec(%q[select date_trunc('hour', created_at), avg(bid) from auctions where item_id = $1 group by 1 order by 1;],params_array)
     buyout_array = []
     bid_array = []
     @buyout_data.entries.each do |x|
       avg_buyout_array = []
       avg_buyout_array.push(x['date_trunc'])
-      avg_buyout_array.push(x['avg'].to_i)
+      avg_buyout_array.push((x['avg'].to_i/10000).floor.abs)
       buyout_array.push(avg_buyout_array)
     end
     @bid_data.entries.each do |x|
       avg_bid_array = []
       avg_bid_array.push(x['date_trunc'])
-      avg_bid_array.push(x['avg'].to_i)
+      avg_bid_array.push((x['avg'].to_i/10000).floor.abs)
       bid_array.push(avg_bid_array)
     end
-    
-    
-     @auction_item = Item.find_by(item_id: params[:item_id])
-     @chart = LazyHighCharts::HighChart.new('graph') do |f|
-          f.title(:text => @auction_item[:name])
-          f.xAxis(
-            type: 'datetime'
-           )
+    @auction_item = Item.find_by(item_id: params[:item_id])
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.title(:text => @auction_item[:name])
+        f.options[:xAxis][:categories] = 7.days*1000
+        # f.xAxis(:type => 'datetime', :pointInterval => 5.days*1000)
+        # f.xAxis [
+        #   {:title => {:text => "Time", :margin => 10} },
+        # ]
+        f.series(:name => "Buyout", :yAxis => 0, :data => buyout_array)
+        f.series(:name => "Bid", :yAxis => 0, :data => bid_array)
+        
+        
+        f.yAxis [
+          {:title => {:text => "Cost in gold", :margin => 10} },
+        ]
 
-          f.series(:name => "Buyout", :yAxis => 0, :data => buyout_array)
-          f.series(:name => "Bid", :yAxis => 0, :data => bid_array)
-          
-          
-          f.yAxis [
-            {:title => {:text => "Revenue", :margin => 70} },
-            {:title => {:text => "Revenue"}, :opposite => true},
-          ]
-
-          f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
-          f.chart({:defaultSeriesType=>"line"})
-        end
-    # @chart = LazyHighCharts::HighChart.new('graph') do |f|
-    #   f.title(:text=> @auction_item[:name] )
-    #   f.options[:xAxis][:categories] = @category
-    #   f.labels(:items=>[:html=>"Total Auction Info", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])      
-    #   f.series(:type=> 'spline',:name=> 'Buyout',:data=> @buyout)
-    #   f.series(:type=> 'spline',:name=> 'Bid',:data=> @bid)
-    #   f.series(:type=> 'spline',:name=> 'Average', :data=> @average)
-    # end
+        f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+        f.chart({:defaultSeriesType=>"line"})
+    end
   end
+
   private
   def params_id
     params.permit(:id,:auc,:item_id,:owner,:ownerRealm,:bid,:buyout,:quantity,:timeLeft,:rand,:seed,:situation,:name)
