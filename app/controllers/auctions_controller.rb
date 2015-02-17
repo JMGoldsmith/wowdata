8048bce3db1389a747e891eae3a3682e3dc00aac
JMGoldsmith <spartanaudio@gmail.com>
2015-02-17 12:35:44 -0600

    active sellers chart

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -10,9 +10,16 @@ class AuctionsController < ApplicationController
     params_array = [] 
     params_array.push(params[:item_id])
     @buyout_data = connection.exec(%q[select date_trunc('day', created_at), avg(buyout) from auctions where item_id = $1 and quantity = 20 group by 1 order by 1;],params_array)
+    @hourly_buyout_data = connection.exec(%q[select date_trunc('hour', created_at), avg(buyout) from auctions where item_id = $1 and quantity = 20 group by 1 order by 1;],params_array)
     @bid_data = connection.exec(%q[select date_trunc('day', created_at), avg(bid) from auctions where item_id = $1 and quantity = 20 group by 1 order by 1;],params_array)
+    @hourly_bid_data = connection.exec(%q[select date_trunc('day', created_at), avg(bid) from auctions where item_id = $1 and quantity = 20 group by 1 order by 1;],params_array)
+    @seller_data = connection.exec(%q[select count(id), owner from auctions where item_id = $1 group by owner order by count desc limit 5;],params_array)
     buyout_array = []
     bid_array = []
+    seller_name_array = []
+    seller_total_array = []
+    hourly_bid_array = []
+    hourly_buyout_array = []
     @buyout_data.entries.each do |x|
       avg_buyout_array = []
       avg_buyout_array.push(x['date_trunc'])
@@ -25,25 +32,62 @@ class AuctionsController < ApplicationController
       avg_bid_array.push((x['avg'].to_i/10000).floor.abs)
       bid_array.push(avg_bid_array)
     end
+    @seller_data.entries.each do |x|
+      seller_total_array.push(x['count'].to_i)
+      seller_name_array.push(x['owner'])
+    end
+    @hourly_buyout_data.entries.each do |x|
+      avg_buyout_array = []
+      avg_buyout_array.push(x['date_trunc'])
+      avg_buyout_array.push((x['avg'].to_i/10000).floor.abs)
+      hourly_buyout_array.push(avg_buyout_array)
+    end
+    @hourly_bid_data.entries.each do |x|
+      avg_buyout_array = []
+      avg_buyout_array.push(x['date_trunc'])
+      avg_buyout_array.push((x['avg'].to_i/10000).floor.abs)
+      hourly_bid_array.push(avg_buyout_array)
+    end
     @auction_item = Item.find_by(item_id: params[:item_id])
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
-        f.title(:text => @auction_item[:name])
-        f.options[:xAxis][:categories] = {pointInterval: 1.day*1000, pointStart: 7.days.ago.getutc.to_i*1000}
-        # f.xAxis(:pointInterval => 1.day, :data => buyout_array)
-        # f.xAxis [
-        #   {:title => {:text => "Time", :margin => 10} },
-        # ]
-
+    @avg_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.title(:text => "#{@auction_item[:name]} average last 10 days" )
+        f.options[:xAxis][:categories] = {pointInterval: 1.day*1000, pointStart: 10.days.ago.getutc.to_i*1000}
         f.series(:name => "Buyout", :yAxis => 0, :data => buyout_array)
         f.series(:name => "Bid", :yAxis => 0, :data => bid_array)
-        
-        
         f.yAxis [
           {:title => {:text => "Cost in gold", :margin => 10} },
         ]
-
         f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
         f.chart({:defaultSeriesType=>"line"})
+    end
+    @hourly_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.title(:text => "#{@auction_item[:name]} average last 24 hours" )
+        f.options[:xAxis][:categories] = {pointInterval: 1.hour*1000, pointStart: 1.day.ago.getutc.to_i*1000}
+        f.series(:name => "Buyout", :yAxis => 0, :data => buyout_array)
+        f.series(:name => "Bid", :yAxis => 0, :data => bid_array)
+        f.yAxis [
+          {:title => {:text => "Cost in gold", :margin => 10} },
+        ]
+        f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+        f.chart({:defaultSeriesType=>"line"})
+    end
+    @seller_chart = LazyHighCharts::HighChart.new('graph') do |f|       
+           f.series(:name=>'Sellers', :data=> seller_total_array)
+           f.title({ :text=>"Most active sellers for #{@auction_item[:name]}"})
+           f.legend({:align => 'right', 
+                    :x => -100, 
+                    :verticalAlign=>'top',
+                    :y=>20,
+                    :floating=>"true",
+                    :backgroundColor=>'#FFFFFF',
+                    :borderColor=>'#CCC',
+                    :borderWidth=>1,
+                    :shadow=>"false"
+                    })
+           f.options[:chart][:defaultSeriesType] = "column"
+           f.options[:xAxis] = {:plot_bands => "none", :title=>{:text=>"Name"}, :categories => seller_name_array}
+           f.options[:yAxis][:title] = {:text=>"Total auctions"}
+          
     end
   end
 
